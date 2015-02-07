6f39097658b2fb3fc7e461d0d416f205256685b3
bredele <olivier.wietrich@gmail.com>
2015-02-06 18:42:21 -0700

    append text nodes to attribute

--- test/attribute.js
+++ test/attribute.js
@@ -4,15 +4,16 @@
  */
 
 var Attribute = require('../lib/attribute');
+var Text = require('../lib/text');
 var assert = require('assert');
 
 
 describe("create attribute", function() {
 
-	it('should create an attribute', function() {
-		var name = new Attribute('name');
-		assert.equal(name.render(), 'name=""');
-	});
+  it('should create an attribute', function() {
+    var name = new Attribute('name');
+    assert.equal(name.render(), 'name=""');
+  });
 
 });
 
@@ -21,7 +22,9 @@ describe("create attribute", function() {
 describe('append child', function() {
 
   it('should append text node', function() {
-
+    var name = new Attribute('name');
+    name.appendChild(new Text('hello'));
+    assert.equal(name.render(), 'name="hello"');
   });
 
 });
