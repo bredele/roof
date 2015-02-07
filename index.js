8fa9f8b5386d667c70adc7a5592e24201d27dc15
bredele <olivier.wietrich@gmail.com>
2015-02-06 19:11:00 -0700

    add doc

--- index.js
+++ index.js
@@ -6,15 +6,48 @@ var Element = require('./lib/element');
 var Attribute = require('./lib/attribute');
 var Text = require('./lib/text');
 
+
+/**
+ * Fake document scope.
+ * @type {Object}
+ */
+
 global.document = {
+
+	/**
+	 * Create Element.
+	 * 
+	 * @param  {String} tag
+	 * @return {Element}
+	 * @api public
+	 */
+	
 	createElement: function(tag) {
 		return new Element(tag);
 	},
 
+
+	/**
+	 * Create Attribute.
+	 * 
+	 * @param  {String} key
+	 * @return {Attribute}
+	 * @api public
+	 */
+	
 	createAttribute: function(key) {
 		return new Attribute(key);
 	},
 
+
+	/**
+	 * Create text node.
+	 * 
+	 * @param  {String} value
+	 * @return {Text}
+	 * @api public
+	 */
+	
 	createTextNode: function(value) {
 		return new Text(value);
 	}
