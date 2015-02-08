4bdbdfa8c03e5aaf49b3c68e21312885d287874e
bredele <olivier.wietrich@gmail.com>
2015-02-08 15:52:33 -0700

    update named node item

---
+++ test/namednodemap.js
@@ -0,0 +1,29 @@
+
+/**
+ * Test dependencies.
+ */
+
+var NamedNodeMap = require('../lib/namednodemap');
+var Node = require('../lib/node');
+var assert = require('assert');
+
+
+describe("named node map", function() {
+
+	it('should set named item', function() {
+		var attributes = new NamedNodeMap();
+		var name = new Node(3, 'name');
+
+		attributes.setNamedItem(name);
+		assert.equal(attributes[0], name);
+	});
+
+	it('should update named item', function() {
+		var attributes = new NamedNodeMap();
+		attributes.setNamedItem(new Node(4, 'name'));
+		var name = new Node(3, 'name');
+		attributes.setNamedItem(name);
+		assert.equal(attributes[0], name);
+	});
+
+});
