793f6233bd5c6eca82dcf796f6d76dae305e6f98
bredele <olivier.wietrich@gmail.com>
2015-02-08 14:44:58 -0700

    append nodes

--- test/node.js
+++ test/node.js
@@ -6,12 +6,23 @@
 var Node = require('../lib/node');
 var assert = require('assert');
 
+describe("node", function() {
+  
+  it('should create node with type', function() {
+    var node = new Node(1);
+    assert.equal(node.nodeType, 1);
+  });
 
-describe("node api", function() {
-	
-	it('should create node with type', function() {
-		var node = new Node(1);
-		assert.equal(node.nodeType, 1);
-	});
+  describe('append node', function() {
+
+    it('should append an other node', function() {
+      var node = new Node(1);
+      node.appendChild(new Node(2));
+
+      assert.equal(node.childNodes.length, 1);
+      assert.equal(node.childNodes[0].nodeType, 2);
+    });
+
+  });
 
 });
