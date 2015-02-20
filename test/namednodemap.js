9e066f4692824e88651e20dd241a869497f659f9
bredele <olivier.wietrich@gmail.com>
2015-02-19 21:03:07 -0700

    attribute get named item

--- test/namednodemap.js
+++ test/namednodemap.js
@@ -18,6 +18,14 @@ describe("named node map", function() {
 		assert.equal(attributes[0], name);
 	});
 
+	it('should get names item', function() {
+		var attributes = new NamedNodeMap();
+		var name = new Node(3, 'name');
+
+		attributes.setNamedItem(name);
+		assert.equal(attributes.getNamedItem('name'), name);
+	});
+
 	it('should update named item', function() {
 		var attributes = new NamedNodeMap();
 		attributes.setNamedItem(new Node(4, 'name'));
